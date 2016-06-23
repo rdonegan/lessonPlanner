85f14146a8de76a0f0a71d6fb993ef744d70f657
Ryan Donegan <r.donegan44@gmail.com>
2016-06-23 11:16:45 +0900

    added markup for swipe delete

--- www/js/lessonList/lessonListController.js
+++ www/js/lessonList/lessonListController.js
@@ -1,20 +1,6 @@
 myApp.onPageInit('lessonList', function (page) {
 
 
-    // TRY something COMPLETELY DIFFERENT WHERE YOU DYNAMICALLY CREATE A LIST USING YOUR OWN JAVASCRIPT ON THE HOME
-    // PAGE AND THEN LINK TO THE LESSONFORM PAGE. 
-
-    // THERE ARE ALSO OTHER DEFAULT WAYS OF DISPLAYING SMART SELECT (POPUP, ETC). MAYBE TRY ACCESSING THEM THIS WAY AND SEE IF THE EFFECT CHANGES
-
-    // $(".list-block").on("click", ".planItem", function(){
-    //     // alert("hello")
-    //     mainView.router.load({
-    //         url: 'lessonForm.html'
-           
-    //     });
-
-
-    // });
       
     
     function getLessons(callback) {
@@ -37,17 +23,14 @@ myApp.onPageInit('lessonList', function (page) {
 
     getLessons(function(items){
 
-
-       // FORGET TEMPLATE7, FILL THAT DATA IN ON YOUR OWN
-
         
         // alert(items[0].standards)
         
         var virtualList= myApp.virtualList('.list-block', {
             items: items,
             renderItem: function(index,item){
-                return '<li>' +
-                        '<a href="lessonForm.html?id='+ item.id+'" class="item-link item-content" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>' +
+                return '<li class="swipeout">' +
+                        '<a href="lessonForm.html?id='+ item.id+'" class="item-link item-content swipeout-content" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>' +
                           '<div class="item-inner">' +
                             '<div class="item-title-row">' +
                               '<div class="item-title">' + item.id + '</div>' +
@@ -55,6 +38,10 @@ myApp.onPageInit('lessonList', function (page) {
                             '<div class="item-subtitle">' + item.subject + '</div>' +
                           '</div>' +
                         '</a>' +
+                        '<div class="swipeout-actions-right">'+
+                            '<a href="#" class="swipeout-delete" data-confirm="Are you sure you want to delete this lesson plan?" data-confirm-title="Delete?">Delete'+
+                            '</a>'+
+                        '</div>'+
                       '</li>';
             }
         // template: '<li class="item-content">' +
