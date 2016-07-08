0c03dd3db6fa6b7efbdd9b8cf6e3c86eda0d02b7
Ryan Donegan <r.donegan44@gmail.com>
2016-07-08 15:35:45 +0900

    migrated sharing methods and everything still WORKS

--- www/js/indexPage/indexController.js
+++ www/js/indexPage/indexController.js
@@ -37,7 +37,125 @@ myApp.onPageInit('index', function (page) {
 });
 
 
+
+//****
+//Sharing lesson plans
+//****
+
+//Share popup is triggered when nav item is tapped
+$$(document).on('click', '.shareLink', function(e){
+    //check that start and endate are both filled in, otherwise, show error
+    if($('.startDateInput').val()=="" || $('.endDateInput').val()=="" ){
+        myApp.alert("No lesson plans shared. Please fill in values for both to and from dates.")
+        return;
+    }
+    else{
+        myApp.showPreloader("Exporting your files");
+        window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function(dir) {
+        // alert("got main dir: " + JSON.stringify(dir));
+        createFile(dir, "log.csv") 
+    });
+
+    }   
+});
+
+
+function createFile(dirEntry, fileName){
+    dirEntry.getFile(fileName, {create: true, exclusive: false}, function(fileEntry){
+        writeFile(fileEntry, null)
+    })
+}
+
+
+//Calls the filewriter to write downloaded csv to file
+//Uses getLessonsByData and jsonToCSV callback methods
+function writeFile(fileEntry, dataObj){
+    //first, get all applicable lessons
+    getLessonsByDate(function(items){
+        var csvItems = jsonToCSV(items)
+        fileEntry.createWriter(function(fileWriter){
+            // alert("still in here")
+            fileWriter.write(csvItems)
+            myApp.hidePreloader();
+            myApp.alert("Records saved as log.csv in your app documents.", "My Planner")
+        })
+    })  
+}
+
+//Helper method that converts JSON results from db query to csv
+//For arrays of strings (e.g. standards, objectives, etc.) it removes commas
+function jsonToCSV(objArray){
+    var header = Object.keys(objArray[0])
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    // var str = '';
+    var str = header + '\r\n';
+
+    // alert(JSON.stringify(array))
+
+    for (var i = 0; i < array.length; i++) {
+        var line = '';
+        for (var index in array[i]) {
+            if (line != ''){ line += ','}
+
+            if (jQuery.type(array[i][index])=="string" && array[i][index].charAt(0) == "["){
+                // alert(array[i][index].charAt(0))
+                var newStr = array[i][index].replace(/,/g, "")
+                line += newStr
+            }
+            else{
+                line += array[i][index];
+            }
+
+           
+            
+        }
+
+        str += line + '\r\n';
+    }
+
+    // alert(str)
+    return str;
+}
+
+//Helper method that reads input in the start and end date fields
+//and returns all lesson plans that fall within that interval
+function getLessonsByDate(callback) {
+        
+    var startDate= $('.startDateInput').val() //get from input
+    var endDate= $('.endDateInput').val() //get from input
+
+    var items = new Array();
+    lpdb.transaction(function(tx) {
+        tx.executeSql('SELECT * FROM lessonplans WHERE startdate >= "' + startDate + '" AND enddate <= "' + endDate +'"', [], function(tx, results) {
+            // alert(results.rows.length)
+            var len = results.rows.length;
+            for (var i=0; i<len; i++){
+                // items.push(results.rows.item(i).subject);
+                var row = {"id": results.rows.item(i).id , "teachername": results.rows.item(i).teachername , "school": results.rows.item(i).school , "startdate": results.rows.item(i).startdate , "enddate": results.rows.item(i).enddate , "grade": results.rows.item(i).grade , "quarter": results.rows.item(i).quarter , "section": results.rows.item(i).section , "subject": results.rows.item(i).subject , "standards": results.rows.item(i).standards , "objectives": results.rows.item(i).objectives , "indicators": results.rows.item(i).indicators , "resources": results.rows.item(i).resources , "notes": results.rows.item(i).notes }
+                items.push(row)
+            }
+            if (items.length==0){
+                myApp.hidePreloader();
+                myApp.alert("Error exporting. No lesson plans saved.", "My Planner")
+
+            }
+            callback(items)
+        }, errorHandler);
+    });
+    function errorHandler(){
+        myApp.hidePreloader();
+        myApp.alert("Error exporting. No lesson plans saved.", "My Planner")
+    }
+}
+
+
+
+//****
+//Get Today's Plans - a callback
+//****
+
 //Returns array of current lesson plans (based on start and end date)
+//Used to populate the list of Today's Plans on the homescreen
 function getCurrentLessons(callback) {
 
     var today = new Date();
