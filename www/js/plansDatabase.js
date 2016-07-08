19cf3afefa6db6029e205bf6ac9fb0a417e7b17c
Ryan Donegan <r.donegan44@gmail.com>
2016-07-08 15:19:57 +0900

    added update function to plansdb.js and it still works

--- www/js/plansDatabase.js
+++ www/js/plansDatabase.js
@@ -46,3 +46,39 @@ function insertLPDB(data){
             })
     })
 }
+
+//Update a record in lessonplans
+function updateLPDB(id, data){
+    var teachername = data.teachername
+    var school = data.school 
+    var startdate = data.startdate 
+    var enddate = data.enddate 
+    var grade = data.grade
+    var quarter = data.quarter
+    var section = data.section
+    var subject = data.subject
+    var standards = JSON.stringify(data.standards)
+    var objectives = JSON.stringify(data.objectives)
+    var indicators = JSON.stringify(data.indicators)
+    var resources = JSON.stringify(data.resources)
+    var subobjectives = JSON.stringify(data.subobjective)
+    var notes = data.notes
+
+    lpdb.transaction(function(tx){
+        // alert("standards: " + standards + " subject: " + subject)
+        var executeQuery = 'UPDATE lessonplans SET teachername = ?, school = ?, startdate = ?, enddate = ?, grade = ?, quarter = ?, section = ?, subject = ?, standards = ?, objectives = ?, indicators = ?, resources = ?, notes = ?, subobjective = ? WHERE ID = "'+ id +'"'
+        // var executeQuery = "INSERT INTO lessonplans (subject) VALUES (?)"
+        tx.executeSql(executeQuery, [teachername, school, startdate, enddate, grade, quarter, section, subject, standards, objectives, indicators, resources, notes, subobjectives],
+            // tx.executeSql("INSERT INTO lessonplans (subject, section, standards) VALUES (?, ?, ?)", [subject, section, standards],
+            function(tx, result){
+                myApp.formDeleteData('lessonForm')
+                mainView.router.loadPage('index.html');
+                myApp.addNotification({
+                    message: 'Successfully updated lesson plan!'
+                });
+            },
+            function(error){
+                myApp.alert("Sorry about that, there was an error and your updates didn't save." )
+            })
+    })
+}
