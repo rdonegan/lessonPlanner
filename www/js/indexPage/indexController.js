6fb5bb7b4d77db3256d5febfaa2bacfd68a9591d
Ryan Donegan <r.donegan44@gmail.com>
2016-07-12 16:39:12 +0900

    removed accordion to troubleshoot UI crashing problem

--- www/js/indexPage/indexController.js
+++ www/js/indexPage/indexController.js
@@ -18,9 +18,9 @@ myApp.onPageInit('index', function (page) {
             var currentPlansList= myApp.virtualList('.currentLessons', {
                 items: items,
                 renderItem: function(index,item){
-                    return '<li class="accordion-item">' +
-                            // '<a href="lessonForm.html?id='+ item.id+'" class="item-link item-content" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>' +
-                             '<a href="#" class="item-link item-content">' + 
+                    return '<li>' +
+                            '<a href="lessonForm.html?id='+ item.id+'" class="item-link item-content" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>' +
+                             // '<a href="#" class="item-link item-content">' + 
                               '<div class="item-inner">' +
                                 '<div class="item-title-row">' +
                                   '<div class="item-title">' + item.subject.charAt(0).toUpperCase() + item.subject.slice(1) + '</div>' +
@@ -32,30 +32,6 @@ myApp.onPageInit('index', function (page) {
                                 '<div class="chip bg-indigo"><div class="chip-label">Performance Indicators: '+JSON.parse(item.indicators).length+'</div></div>'+
                               '</div>' +
                             '</a>' +
-                            '<div class="accordion-item-content">' +
-                                '<div class="card">' +
-                                  '<div class="card-header">Lesson Sequence</div>' +
-                                  '<div class="card-content">' +
-                                    '<div class="card-content-inner"> ' + 
-                                        '<p>'+ ((item.sequence) ? item.sequence:"No class sequence added. Tap 'edit' to add one now.") +'</p>' +
-                                        '<p>' + JSON.parse(item.resources) + '</p>' +
-                                    '</div>' +
-                                  '</div>' +
-                                '</div>' +
-                                '<div class="card">' +
-                                  '<div class="card-header">Curriculum Goals</div>' +
-                                  '<div class="card-content">' +
-                                    '<div class="card-content-inner"> ' + 
-                                        '<p>Standards: '+ ((JSON.parse(item.standards).length > 0) ? JSON.parse(item.standards):"No standards added.") +'</p>' +
-                                        '<p>Objectives: '+ ((JSON.parse(item.objectives).length > 0) ? JSON.parse(item.objectives):"No objectives added.") +'</p>' +
-                                        '<p>Indicators: '+ ((JSON.parse(item.indicators).length > 0) ? JSON.parse(item.indicators):"No indicators added.") +'</p>' +
-                                    '</div>' +
-                                    '<div class="card-footer">' +
-                                    '<a href="lessonForm.html?id='+ item.id+'" class="button button-big button-fill color-pink item-link" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>Edit</a>' +
-                                    '</div>' +
-                                  '</div>' +
-                                '</div>' +
-                            '</div>'+
                           '</li>';
                 },
                 height:115
@@ -64,6 +40,88 @@ myApp.onPageInit('index', function (page) {
     }  
 
   })
+
+
+// '<li class="accordion-item">' +
+//                             // '<a href="lessonForm.html?id='+ item.id+'" class="item-link item-content" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>' +
+//                              '<a href="#" class="item-link item-content">' + 
+//                               '<div class="item-inner">' +
+//                                 '<div class="item-title-row">' +
+//                                   '<div class="item-title">' + item.subject.charAt(0).toUpperCase() + item.subject.slice(1) + '</div>' +
+//                                   '<div class="item-after">'+toMonth((item.startdate.substr(5,5)).substr(0,2))+ ' ' + item.startdate.substr(0,4) + '</div>' +
+//                                 '</div>' +
+//                                 '<div class="item-subtitle">Grade ' + item.grade +', Quarter '+ item.quarter + '</div>' +
+//                                 '<div class="chip bg-teal"><div class="chip-label">Standards: '+JSON.parse(item.standards).length+'</div></div>'+
+//                                 '<div class="chip bg-amber"><div class="chip-label">Objectives: '+JSON.parse(item.objectives).length+'</div></div>'+
+//                                 '<div class="chip bg-indigo"><div class="chip-label">Performance Indicators: '+JSON.parse(item.indicators).length+'</div></div>'+
+//                               '</div>' +
+//                             '</a>' +
+//                             '<div class="accordion-item-content">' +
+//                                 '<div class="card">' +
+//                                   '<div class="card-header">Lesson Sequence</div>' +
+//                                   '<div class="card-content">' +
+//                                     '<div class="card-content-inner"> ' + 
+//                                         '<p>'+ ((item.sequence) ? item.sequence:"No class sequence added. Tap 'edit' to add one now.") +'</p>' +
+//                                         '<p>' + JSON.parse(item.resources) + '</p>' +
+//                                     '</div>' +
+//                                   '</div>' +
+//                                 '</div>' +
+//                                 '<div class="card">' +
+//                                   '<div class="card-header">Curriculum Goals</div>' +
+//                                   '<div class="card-content">' +
+//                                     '<div class="card-content-inner"> ' + 
+//                                         '<p>Standards: '+ ((JSON.parse(item.standards).length > 0) ? JSON.parse(item.standards):"No standards added.") +'</p>' +
+//                                         '<p>Objectives: '+ ((JSON.parse(item.objectives).length > 0) ? JSON.parse(item.objectives):"No objectives added.") +'</p>' +
+//                                         '<p>Performance Indicators: '+ ((JSON.parse(item.indicators).length > 0) ? JSON.parse(item.indicators):"No indicators added.") +'</p>' +
+//                                     '</div>' +
+//                                     '<div class="card-footer">' +
+//                                     '<a href="lessonForm.html?id='+ item.id+'" class="button button-big button-fill color-pink item-link" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>Edit</a>' +
+//                                     '</div>' +
+//                                   '</div>' +
+//                                 '</div>' +
+//                             '</div>'+
+//                           '</li>';
+
+
+
+    //****
+    //Get Today's Plans - a callback
+    //****
+
+    //Returns array of current lesson plans (based on start and end date)
+    //Used to populate the list of Today's Plans on the homescreen
+    function getCurrentLessons(callback) {
+
+        var today = new Date();
+        var dd = today.getDate();
+        var mm = today.getMonth()+1; //January is 0!
+        var yyyy = today.getFullYear();
+
+        if(dd<10) {
+            dd='0'+dd
+        } 
+        if(mm<10) {
+            mm='0'+mm
+        } 
+
+        today =  yyyy+'-'+mm+'-'+dd;
+        // alert("today's date: " + today)
+            
+        var items = new Array();
+        lpdb.transaction(function(tx) {
+            tx.executeSql('SELECT * FROM lessonplans WHERE startdate <= "' + today + '" AND enddate >= "' + today +'" ORDER BY date(startdate)', [], function(tx, results) {
+                
+                var len = results.rows.length;
+                for (var i=0; i<len; i++){
+                    // items.push(results.rows.item(i).subject);
+                    var row = {"id": results.rows.item(i).id , "teachername": results.rows.item(i).teachername , "school": results.rows.item(i).school , "startdate": results.rows.item(i).startdate , "enddate": results.rows.item(i).enddate , "grade": results.rows.item(i).grade , "quarter": results.rows.item(i).quarter , "section": results.rows.item(i).section , "subject": results.rows.item(i).subject , "standards": results.rows.item(i).standards , "objectives": results.rows.item(i).objectives , "indicators": results.rows.item(i).indicators , "resources": results.rows.item(i).resources , "notes": results.rows.item(i).notes, "sequence": results.rows.item(i).sequence, "subobjectives": results.rows.item(i).subobjective }
+                    items.push(row)
+                }
+                
+                callback(items)
+            });
+        });
+    }
 
 
 
@@ -216,50 +274,6 @@ function getLessonsByDate(callback) {
         myApp.alert("Error exporting. No lesson plans saved.", "My Planner")
     }
 }
-
-
-
-
-//****
-//Get Today's Plans - a callback
-//****
-
-//Returns array of current lesson plans (based on start and end date)
-//Used to populate the list of Today's Plans on the homescreen
-function getCurrentLessons(callback) {
-
-    var today = new Date();
-    var dd = today.getDate();
-    var mm = today.getMonth()+1; //January is 0!
-    var yyyy = today.getFullYear();
-
-    if(dd<10) {
-        dd='0'+dd
-    } 
-    if(mm<10) {
-        mm='0'+mm
-    } 
-
-    today =  yyyy+'-'+mm+'-'+dd;
-    // alert("today's date: " + today)
-        
-    var items = new Array();
-    lpdb.transaction(function(tx) {
-        tx.executeSql('SELECT * FROM lessonplans WHERE startdate <= "' + today + '" AND enddate >= "' + today +'" ORDER BY date(startdate)', [], function(tx, results) {
-            
-            var len = results.rows.length;
-            for (var i=0; i<len; i++){
-                // items.push(results.rows.item(i).subject);
-                var row = {"id": results.rows.item(i).id , "teachername": results.rows.item(i).teachername , "school": results.rows.item(i).school , "startdate": results.rows.item(i).startdate , "enddate": results.rows.item(i).enddate , "grade": results.rows.item(i).grade , "quarter": results.rows.item(i).quarter , "section": results.rows.item(i).section , "subject": results.rows.item(i).subject , "standards": results.rows.item(i).standards , "objectives": results.rows.item(i).objectives , "indicators": results.rows.item(i).indicators , "resources": results.rows.item(i).resources , "notes": results.rows.item(i).notes, "sequence": results.rows.item(i).sequence, "subobjectives": results.rows.item(i).subobjective }
-                items.push(row)
-            }
-            
-            callback(items)
-        });
-    });
-}
-
-
 
 //****
 //Update CURRICULUM db
