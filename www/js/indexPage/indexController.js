eec8f20e816c2e6f6168f2ca050a735a345ccd88
Ryan Donegan <r.donegan44@gmail.com>
2016-07-20 14:49:48 +0900

    welcomescreen only opens on first run now

--- www/js/indexPage/indexController.js
+++ www/js/indexPage/indexController.js
@@ -1,24 +1,31 @@
 myApp.onPageInit('index', function (page) {
 
-    var options = {
-      'bgcolor': '#0da6ec',
-      'fontcolor': '#fff'
+
+    //if it's the first run of the app, open welcomescreen
+    if (window.localStorage.getItem("loggedIn") != 1){
+        window.localStorage.setItem("loggedIn", 1)
+        var options = {
+          'bgcolor': '#0da6ec',
+          'fontcolor': '#fff'
+        }
+
+        var welcomescreen_slides = [
+          {
+            id: 'slide0',
+            picture: '<div class="tutorialicon">♥</div>',
+            text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
+          },
+          {
+            id: 'slide1',
+            picture: '<div class="tutorialicon">✲</div>',
+            text: '<a class="tutorial-close-btn" href="#">End Tutorial</a>'
+          }
+        ];
+        
+        var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
     }
 
-    var welcomescreen_slides = [
-      {
-        id: 'slide0',
-        picture: '<div class="tutorialicon">♥</div>',
-        text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
-      },
-      {
-        id: 'slide1',
-        picture: '<div class="tutorialicon">✲</div>',
-        text: '<a class="tutorial-close-btn" href="#">End Tutorial</a>'
-      }
-    ];
-
-    var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
+    
 
     $$(document).on('click', '.tutorial-close-btn', function () {
       welcomescreen.close();
