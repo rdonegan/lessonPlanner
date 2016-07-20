6a9e04378d8f2a07e2626dfc8a1649bb22e5d4b7
Ryan Donegan <r.donegan44@gmail.com>
2016-07-20 14:45:08 +0900

    got tutorial slides to show up

--- www/js/indexPage/indexController.js
+++ www/js/indexPage/indexController.js
@@ -1,5 +1,29 @@
 myApp.onPageInit('index', function (page) {
 
+    var options = {
+      'bgcolor': '#0da6ec',
+      'fontcolor': '#fff'
+    }
+
+    var welcomescreen_slides = [
+      {
+        id: 'slide0',
+        picture: '<div class="tutorialicon">♥</div>',
+        text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
+      },
+      {
+        id: 'slide1',
+        picture: '<div class="tutorialicon">✲</div>',
+        text: '<a class="tutorial-close-btn" href="#">End Tutorial</a>'
+      }
+    ];
+
+    var welcomescreen = myApp.welcomescreen(welcomescreen_slides, options);
+
+    $$(document).on('click', '.tutorial-close-btn', function () {
+      welcomescreen.close();
+    });
+
     //only show export to email option if device is configured with email
     if (cordova.plugins.email.isAvailable){
         $('.emailShare').removeClass("hidden");
