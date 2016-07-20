5a4e2bf2bba417f85d56756a2e68fa16fb55684b
Ryan Donegan <r.donegan44@gmail.com>
2016-07-20 12:18:45 +0900

    added prompt to homescreen if no plans

--- www/js/indexPage/indexController.js
+++ www/js/indexPage/indexController.js
@@ -1,6 +1,5 @@
 myApp.onPageInit('index', function (page) {
 
- 
     //only show export to email option if device is configured with email
     if (cordova.plugins.email.isAvailable){
         $('.emailShare').removeClass("hidden");
@@ -8,11 +7,11 @@ myApp.onPageInit('index', function (page) {
 
   //index page is initialized
   getCurrentLessons(function(items){
-
     if (items.length==0){
         return
     }
     else{
+        $('.openingPrompt').addClass("hidden");
         var currentPlansList= myApp.virtualList('.currentLessons', {
             items: items,
             renderItem: function(index,item){
@@ -47,8 +46,9 @@ myApp.onPageInit('index', function (page) {
                                     '</div>'+
                                     '<div class="row">'+
                                         '<div class="col-20"><div class="chip bg-deeppurple"><div class="chip-label">Resource Details</div></div></div>'+
-                                        '<div class="col-80"><ul class="itemList"><li>'+((item.sequence) ? item.sequence:"No additional resources added. Tap 'edit' to add some now.")+'</li></ul></div>' +
+                                        '<div class="col-80"><ul class="itemList"><li>'+((item.sequence) ? item.sequence:"No additional resources added. Tap 'edit' to add more.")+'</li></ul></div>' +
                                     '</div>'+
+                                    '<br>'+
                                     '<div class="row"><a href="lessonForm.html?id='+ item.id+'" class="col-20 button button-fill color-pink item-link" data-context=\'{"standards":' + item.standards +', "objectives": ' + item.objectives +' }\'>Edit</a></div>' +
                                 '</div>'+
                             '</div>'+ 
