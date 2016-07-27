2461d82b4522479204639e6df62ede1982e6c67c
Ryan Donegan <r.donegan44@gmail.com>
2016-07-27 14:36:17 +0900

    removed unused files

--- www/js/lessonForm/lessonFormController.js
+++ www/js/lessonForm/lessonFormController.js
@@ -464,6 +464,18 @@ myApp.onPageInit('lessonForm', function(page){
         })
     }
 
+    // $("#textarea").focus(function(e) {
+    //     var container = $('#container'),
+    //         scrollTo = $('#textarea');
+
+    //     setTimeout((function() {
+    //         container.animate({
+    //         scrollTop: scrollTo.offset().top - container.offset().top + container.scrollTop()
+    //         });
+    //     }), 500);
+
+    // });
+
 });
 
 
